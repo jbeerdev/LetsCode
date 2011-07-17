f0256007cf1cdd23502d89e8943757a2878589b5
jbeerdev <jbeerbros@gmail.com>
2011-07-17 14:11:13 +0200

    CC added

--- src/lets/code/project/background/WebServiceAsyncTask.java
+++ src/lets/code/project/background/WebServiceAsyncTask.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Este código está bajo la licencia CC by sa 
+ * http://creativecommons.org/licenses/by-sa/2.5/es/
+ * 
+ * Licencia que le permite copiar, distribuir y comunicar públicamente la obra, transformar la obra o hacer un uso comercial de esta obra siempre y cuando cumpla las condiciones de:
+ * Reconocimiento — Debe reconocer los créditos de la obra de la manera especificada por el autor Jose B. Cortés (pero no de una manera que sugiera que tiene su apoyo o apoyan el uso que hace de su obra).
+ * 
+ * Compartir bajo la misma licencia — Si altera o transforma esta obra, o genera una obra derivada, sólo puede distribuir la obra generada bajo una licencia idéntica a ésta.
+ ******************************************************************************/
 package lets.code.project.background;
 
 import java.lang.reflect.Type;
