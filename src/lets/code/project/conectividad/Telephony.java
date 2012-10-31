b2f0af313c8e397aa909aa4d9cff8dee5ea61e07
jbeerdev <jbeerbros@gmail.com>
2012-10-31 13:06:08 +0100

    Small change

--- src/lets/code/project/conectividad/Telephony.java
+++ src/lets/code/project/conectividad/Telephony.java
@@ -5,6 +5,7 @@ import android.telephony.TelephonyManager;
 
 public class Telephony {
 
+
 	/* How to check if we are in roaming*/
 	public static boolean isRoaming(Context context){
 		TelephonyManager telephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);  
